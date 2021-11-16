82f45f531b07ac5e7a3d4986e7bf907328396456
TechXero *~* DarkXero <81101439+TechXero@users.noreply.github.com>
2021-11-16 16:58:45 +0200

    Update SetupRice.sh

--- SetupRice.sh
+++ SetupRice.sh
@@ -10,5 +10,7 @@ git clone https://github.com/xerolinux/xero-layan-git.git
 sleep 2
 echo "Applying Connversion Rice"
 echo "#################################"
-cd xero-layan-git && sh installDebian.sh
+cd xero-layan-git
+sudo sed -i "s/latte-dock/sublime-text/g" installDebian.sh
+sh installDebian.sh
 sleep 2
