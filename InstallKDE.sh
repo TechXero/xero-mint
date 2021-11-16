e8c0a80ddb368c054a108f99bd5cf34d94cb9332
TechXero *~* DarkXero <81101439+TechXero@users.noreply.github.com>
2021-11-16 14:06:44 +0200

    Update InstallKDE.sh

--- InstallKDE.sh
+++ InstallKDE.sh
@@ -17,10 +17,6 @@ echo "Installing KDE (Select SDDM)"
 echo "#################################"
 sudo apt install -y kde-plasma-desktop
 sleep 2
-echo "Installing Few Required Packages"
-echo "#################################"
-sudo apt install -y git cmake extra-cmake-modules qtdeclarative5-dev libqt5x11extras5-dev libkf5iconthemes-dev libkf5plasma-dev libkf5windowsystem-dev libkf5declarative-dev libkf5xmlgui-dev libkf5activities-dev build-essential libxcb-util-dev libkf5wayland-dev git gettext libkf5archive-dev libkf5notifications-dev libxcb-util0-dev libsm-dev libkf5crash-dev libkf5newstuff-dev libxcb-shape0-dev libxcb-randr0-dev libx11-dev libx11-xcb-dev kirigami2-dev
-sleep 2
 echo "##########################################"
 echo "Done ! Plasma Session availabe on login"
 echo "##########################################"
